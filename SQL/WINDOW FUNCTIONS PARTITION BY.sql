37563e2a80ac5b3e40bf5bb5f3a1ff6c0edc30f1
Homeros200 <george.gomer@gmail.com>
2026-06-22 21:23:35 -0400

    update

---
+++ SQL/WINDOW FUNCTIONS PARTITION BY.sql
@@ -0,0 +1,17 @@
+-- Find the total sales across all orders
+--Find the total sales for each product
+--Find the total sales for each combination of product and order status
+--Additionally provide details such order Id, order date
+
+
+SELECT
+OrderID,
+OrderDate,
+ProductID,
+OrderStatus,
+Sales,
+SUM(Sales) OVER() TotalSales,
+SUM(Sales) OVER(PARTITION BY ProductID) TotalSalesByProducts,
+SUM(Sales) OVER (PARTITION BY ProductID, OrderStatus) SalesByProductsAndStatus
+FROM Sales.Orders
+
