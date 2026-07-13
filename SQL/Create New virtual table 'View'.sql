d11ad464f0e944c4938b3a076968ede7fecf1da6
Homeros200 <george.gomer@gmail.com>
2026-07-13 16:30:15 -0400

    update

---
+++ SQL/Create New virtual table 'View'.sql
@@ -0,0 +1,11 @@
+	
+	CREATE VIEW V_Monthly_Summary AS
+	(
+	SELECT 
+	DATETRUNC(month, OrderDate) OrderMonth,
+	SUM(Sales) TotalSales,
+	COUNT (OrderID) TotalOrders,
+	SUM(Quantity) TotalQuantities
+	FROM Sales.Orders
+	GROUP BY DATETRUNC(month, OrderDate)
+	)
