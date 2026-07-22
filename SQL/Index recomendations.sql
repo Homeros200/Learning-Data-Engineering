f3ac490bcb895e55b9df0022f383b8099998dd60
Homeros200 <george.gomer@gmail.com>
2026-07-22 13:42:37 -0400

    update

---
+++ SQL/Index recomendations.sql
@@ -0,0 +1,12 @@
+SELECT 
+fs.SalesOrderNumber,
+dp.EnglishProductName,
+dp.Color,
+fs.OrderDateKey
+FROM FactInternetSales fs
+INNER JOIN DimProduct dp
+ON fs.ProductKey = dp.ProductKey
+WHERE dp.Color= 'Black'
+AND fs.OrderDateKey BETWEEN 20101229 AND 20101231
+
+SELECT * FROM sys.dm_db_missing_index_details
